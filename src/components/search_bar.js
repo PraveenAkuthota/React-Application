db6c255194cf3a9252e5f3f0d1bcdd0bbb0d8492
praveenaku1234 <praveenaku1234@gmail.com>
2017-11-05 03:46:35 -0500

    Handling User Events.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';  //const Component = React.Component
 
 class SearchBar extends Component {
     render() {   // Every Class based Components should have a render method.
-        return <input />;
+        return <input onChange={this.handleInputChange} />;
     }
+
+     handleInputChange(event){
+        console.log(event.target.value);
+     }
 }
 
 export default SearchBar;
