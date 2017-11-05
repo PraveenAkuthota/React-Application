9c3769a637de851639ddf0cf9a05552ae0f2b3f7
praveenaku1234 <praveenaku1234@gmail.com>
2017-11-05 03:49:10 -0500

    Modifying the Handler function to arrow function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,12 +2,8 @@ import React, { Component } from 'react';  //const Component = React.Component
 
 class SearchBar extends Component {
     render() {   // Every Class based Components should have a render method.
-        return <input onChange={this.handleInputChange} />;
+        return <input onChange = { (event) => {console.log(event.target.value)}} />;
     }
-
-     handleInputChange(event){
-        console.log(event.target.value);
-     }
 }
 
 export default SearchBar;
