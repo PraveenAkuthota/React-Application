011804776a1280b1a1d0cd7f26fe6c7fa5e763fc
praveenaku1234 <praveenaku1234@gmail.com>
2017-11-05 02:02:00 -0500

    Creating Search bar and rendering it in the index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-import React,  { Component } from 'react';    //React knows how to render the components/elemts, i.e. manage components
+import React, { Component } from 'react';    //React knows how to render the components/elemts, i.e. manage components
 import ReactDOM from 'react-dom';// Render's react component to DOM, Basically used to render DOM
+import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDuFtZHAHs9frZTimVBFoP4Pti6ZkKSE7I';
 
 // Create a component which produces some html
 const App = () => {  //const App = function(){
-     // App is constant because we dont want this variable to change again in future.
-     return <div>Hi!!!</div>;
+    // App is constant because we dont want this variable to change again in future.
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
 }
 
- // Take this component's generated html and put it on page (DOM)
+// Take this component's generated html and put it on page (DOM)
 
- ReactDOM.render(<App />, document.querySelector('.container')); //We changed from App to <App /> because we need to pass component's instance not the component. So to make it istances we need to place them inside the JSX Tags.
+ReactDOM.render(<App />, document.querySelector('.container')); //We changed from App to <App /> because we need to pass component's instance not the component. So to make it istances we need to place them inside the JSX Tags.
 
 // instead of <App></App> we can just write <App /> if there's nothing in between.
 // First argument says what to render and second argument says where to render this component.
