99c257b596c72a57679df930b39dfbf980f72fe9
praveenaku1234 <praveenaku1234@gmail.com>
2017-11-05 01:30:56 -0500

    Creating First Component to Render Hi on the page.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React,  { Component } from 'react';    //React knows how to render the components/elemts, i.e. manage components
+import ReactDOM from 'react-dom';// Render's react component to DOM, Basically used to render DOM
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a component which produces some html
+const App = () => {  //const App = function(){
+     // App is constant because we dont want this variable to change again in future.
+     return <div>Hi!!!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+ // Take this component's generated html and put it on page (DOM)
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ ReactDOM.render(<App />, document.querySelector('.container')); //We changed from App to <App /> because we need to pass component's instance not the component. So to make it istances we need to place them inside the JSX Tags.
+
+// instead of <App></App> we can just write <App /> if there's nothing in between.
+// First argument says what to render and second argument says where to render this component.
+// Second argument is a reference to the existing DOM element on the page. So what we are saying is that Render the component and produce some HTML and insert that html into second argument.
+
