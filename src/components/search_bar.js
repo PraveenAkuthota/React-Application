aa45a4df87a1a3ebc84a6b3b8d7360cc488faa19
praveenaku1234 <praveenaku1234@gmail.com>
2017-11-05 04:04:52 -0500

    Defining State

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';  //const Component = React.Component
 
 class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };  // Only Class-Based Components have state
+    }
+
     render() {   // Every Class based Components should have a render method.
         return <input onChange = { (event) => {console.log(event.target.value)}} />;
     }
