3eb2ae04177506cce73e795d725ead8e1f3f5def
praveenaku1234 <praveenaku1234@gmail.com>
2017-11-05 04:21:41 -0500

    Controlled Form Components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,15 +5,17 @@ class SearchBar extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { term: '' };  // Only Class-Based Components have state
+        this.state = { term: 'Praveen' };  // Only Class-Based Components have state
     }
 
     render() {   // Every Class based Components should have a render method.
         return (
             <div>
-        <input onChange = { (event) => this.setState({ term: event.target.value})} />
-        Value of the Input is : {this.state.term}
-        </div>
+                <input
+                    // Controlled Form Element means the input value is set by the state. So Value changes only when the state changes.
+                    value={this.state.term}
+                    onChange={(event) => this.setState({ term: event.target.value })} />
+            </div>
         );
     }
 }
