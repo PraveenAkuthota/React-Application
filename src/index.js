d3c56f203a1f22e49ecac30b889d6d85c7bc6c09
praveenaku1234 <praveenaku1234@gmail.com>
2017-11-07 00:27:11 -0500

    On Video Select from the Video List

--- src/index.js
+++ src/index.js
@@ -14,23 +14,31 @@ class App extends Component {  //const App = function(){  // App is constant bec
 
     constructor(props) {
         super(props);
-        this.state = { videos: [] };
+        this.state = {
+             videos: [],
+            selectedVideo: null
+         };
 
         YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
-            this.setState({ videos });    // this.setState({ videos : videos }); // When kEy and Value are same we can just use one name
+            this.setState({
+                 videos: videos,
+                selectedVideo: videos[0]
+             });    // this.setState({ videos : videos }); // When kEy and Value are same we can just use one name
         });
 
         // YTSearch({ key: API_KEY, term: 'surfboards' }, (data) => {
         //    this.setState({videos : data});
-        // });
+        // }); 
     }
 
     render() {
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]} />
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                onVideoSelect = { selectedVideo => this.setState({selectedVideo})}
+                videos={this.state.videos} />
             </div>
         )
     }
